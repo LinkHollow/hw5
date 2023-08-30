714e6da24f8b98730d1a04324e35141f1a386fbe
Egor Sazhnev <hard-link@mail.ru>
2023-08-30 18:07:15 +1000

    done

---
+++ hw5.sql
@@ -0,0 +1,29 @@
+use semimar_4;
+/*. Создайте представление, в которое попадет информация о пользователях (имя, фамилия, город и пол), которые не старше 20 лет. */
+CREATE OR REPLACE VIEW view_messages
+AS 
+(SELECT firstname, lastname, gender, hometown
+FROM users
+JOIN profiles ON uSers.id = profiles.user_id 
+WHERE TIMESTAMPDIFF(YEAR, birthday, CURDATE()) <= 20);
+
+SELECT *
+FROM view_messages;
+
+/*2.  Найдите кол-во, отправленных сообщений каждым пользователем и выведите
+ранжированный список пользователей, указав имя и фамилию пользователя, количество
+отправленных сообщений и место в рейтинге (первое место у пользователя с максимальным количеством сообщений) . (используйте DENSE_RANK)*/
+SELECT users.id, firstname, lastname, COUNT(messages.body) AS send_messages,
+DENSE_RANK() OVER(ORDER BY COUNT(messages.body) DESC) rank_messages
+FROM users
+JOIN messages ON users.id = messages.from_user_id
+GROUP BY users.id;
+
+
+/*Выберите все сообщения, отсортируйте сообщения по возрастанию даты отправления (created_at) 
+и найдите разницу дат отправления между соседними сообщениями, получившегося списка. (используйте LEAD или LAG)*/
+
+SELECT body AS message, created_at AS 'date of create',
+timestampdiff(minute, lag(created_at) OVER(ORDER BY created_at), created_at) AS 'difference time, minutes' 
+FROM messages
+ORDER BY created_at;
